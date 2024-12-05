d21cb295b759ca5291083a5a1772b70d325f7756
kmile <k.milev@yahoo.com>
2024-12-05 22:10:25 +0200

    Http response with last data

--- src/station-data/station-data.controller.ts
+++ src/station-data/station-data.controller.ts
@@ -4,6 +4,7 @@ import type { Response } from 'express';
 import { StationListService } from './station-list.service';
 import { StationDataWithInfoDto } from './dto/station-data-info.dto';
 import { StationInfoService } from './station-info.service';
+import { StationData } from './entities/station-data.entity';
 
 enum ERRORS {
   'WMS_NOT_FOUND' = 'WMS ID not found. Please, check the ID!',
@@ -30,6 +31,21 @@ export class StationDataController {
       data.info = this.stationInfoService.getStationInfo(id);
       data.stationId = id;
       data.stationData = this.stationDataService.findOne(id);
+      res.status(HttpStatus.OK).json(data);
+    } else {
+      res.status(HttpStatus.NOT_FOUND).json({ error: ERRORS.WMS_NOT_FOUND });
+    }
+  }
+  @Get('last/:id')
+  findLastOne(@Param('id') id: string, @Res() res: Response) {
+    if (this.stations.includes(+id)) {
+      const data: StationData = new StationData();
+      const lastData = this.stationDataService.findLastOne(id);
+      data.eventTime = lastData.eventTime;
+      data.temperature = lastData.temperature;
+      data.humidity = lastData.humidity;
+      data.distance = lastData.distance;
+      data.light = lastData.light;
       res.status(HttpStatus.OK).json(data);
     } else {
       res.status(HttpStatus.NOT_FOUND).json({ error: ERRORS.WMS_NOT_FOUND });
